030e1ad69da118461c46e8ac06c2b1e26db5ee4e
Ben Lemus <benlemus@Bens-MacBook-Pro-2.local>
2024-12-05 16:20:52 -0800

    working app... for the most part

--- app.py
+++ app.py
@@ -1,18 +1,66 @@
 from boggle import Boggle
-from flask import Flask, render_template
+from flask import Flask, render_template, session, jsonify, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+import time
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mydogmilo03'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 app.debug = True
 toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
-@app.route('/')
-def show_board():
-    board = boggle_game.make_board()
+highscore = {}
 
-    return render_template('game.html', board=board)
+@app.route('/')
+def show_board(): 
+    if 'board' not in session:
+        game_board = boggle_game.make_board()
+        session['board'] = game_board
+        session['result'] = ''
+        session['points'] = 0
+        session['highscores'] = {}
+        session['game_num'] = 1
+    
+    return render_template('game.html', board=session['board'])
+
+def get_points(guess, result):
+    if result == 'ok':
+        return len(guess)
+    else:
+        return 0
+        
+@app.route('/handle-form', methods=["POST"])
+def handle_form():
+    '''Starts a 60 second countdown and ends guessing when 0.'''
+    guess = request.json.get('guess')
+    session['result'] = boggle_game.check_valid_word(session['board'], guess)
+
+    session['points'] += get_points(guess, session['result'])
+    
+    return jsonify({'result': session['result'], 'points': session['points']}) 
+
+@app.route('/update-highscores')
+def get_highscores():
+    
+    session['highscores'] = dict(sorted(highscore.items(), key=lambda x:x[1], reverse=True))
+    
+    return jsonify(session['highscores'])
+
+
+@app.route('/reset-game')
+def reset_game():
+    get_num = session['game_num']
+    highscore[f'{get_num}'] = session['points']
+
+    game_board = boggle_game.make_board()
+    session['board'] = game_board
+    session['result'] = ''
+    session['points'] = 0
+    session['game_num'] += 1
+    return redirect('/')
+
+
---
+++ static/app.js
@@ -0,0 +1,79 @@
+const guess = document.querySelector("#guess");
+const form = document.querySelector(".guessForm");
+const formBtn = document.querySelector("#submitBtn");
+const show_timer = document.querySelector("#timer");
+const resetForm = document.querySelector(".resetForm");
+
+let count = 60;
+let timer_status = "deactive";
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (timer_status == "deactive") {
+    const timer = setInterval(function () {
+      count--;
+      show_timer.innerText = count;
+
+      if (count == 0) {
+        formBtn.disabled = true;
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+
+  timer_status = "active";
+
+  axios
+    .post("/handle-form", { guess: guess.value })
+    .then((res) => {
+      const data = res.data.result;
+      const points = res.data.points;
+
+      const points_h3 = document.querySelector("#points");
+
+      points_h3.innerText = "Points: " + points;
+
+      result = document.querySelector("#result");
+      if (data == "ok") {
+        result.innerText = "Correct!";
+      } else if (data == "not-on-board") {
+        result.innerText = "Word not on board";
+      } else {
+        result.innerText = "Not a valid word";
+      }
+    })
+    .catch((error) => {
+      console.log("Error fetching data", error);
+    });
+});
+
+function update_highscore() {
+  axios
+    .get("/update-highscores")
+    .then(function (res) {
+      let data = res.data;
+      let tBody = document.querySelector("#tableBody");
+
+      tBody.innerHTML = "";
+
+      for (const [game, point] of Object.entries(data)) {
+        const newRow = document.createElement("tr");
+
+        const gameCell = document.createElement("td");
+        gameCell.innerText = game;
+
+        const pointCell = document.createElement("td");
+        pointCell.innerText = point;
+
+        newRow.appendChild(gameCell);
+        newRow.appendChild(pointCell);
+        tBody.appendChild(newRow);
+      }
+    })
+    .catch((err) => {
+      console.error("Error fetching high scores:", err);
+    });
+}
+
+update_highscore();
